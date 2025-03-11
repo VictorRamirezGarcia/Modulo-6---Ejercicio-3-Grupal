1a9242261ac9a6e26d8e7be176557e3a0fe39468
anemonadja <nadja.villarroel.n@gmail.com>
2025-03-10 21:02:18 -0300

    icons in manifest

--- mi-pwa/dev-dist/sw.js
+++ mi-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t5sq0bskdr8"
+    "revision": "0.0lq2k8r3hog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
