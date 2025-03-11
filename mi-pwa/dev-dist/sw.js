541fa8c838bf9283005e158ce701e61ada4e7bae
anemonadja <nadja.villarroel.n@gmail.com>
2025-03-10 21:06:59 -0300

    revision de 0.0lq2k8r3hog a 0.1ql3gp4hqn

--- mi-pwa/dev-dist/sw.js
+++ mi-pwa/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0lq2k8r3hog"
+    "revision": "0.1ql3gp4hqn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
